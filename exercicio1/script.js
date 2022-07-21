7b6396c6ae1dadc3548e9e44cb0f9789acd78650
PedroMagCoder <pedromagno999@gmail.com>
2022-07-21 18:10:07 -0300

    exercícios aula 8

---
+++ exercicio1/script.js
@@ -0,0 +1,4 @@
+let numeroPar = + prompt('insira um número par')
+console.log(numeroPar / 2)
+
+// A operação roda corretamente, ao inserir um úmero ímpar o resultado é um número decimal //
---
+++ exercicio3/script.js
@@ -0,0 +1,13 @@
+console.log( 20 < 5 && 5 < 2 )
+console.log( 5 === 5 || 5 === "5")
+console.log(!20 > 50)
+console.log(  !(20 > 50))
+
+
+
+
+
+
+
+
+
